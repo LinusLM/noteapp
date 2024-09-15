57e21f1640326c19733bcca0aa40489f77ae4783
LinusLM <linuslmathiesen@icloud.com>
2024-09-15 12:14:01 +0200

    test

---
+++ src/livechat.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Intercom from '@intercom/messenger-js-sdk';
+
+export default function Component() {
+  Intercom({
+    app_id: 'jaeuegoi',
+  });
+
+  return <div>Example App</div>;
+}
